16941cd2262670d04f259e1ddc742683a45f8fc4
Daniel Drizhuk <d.drizhuk@gmail.com>
2023-06-21 22:16:46 +0200

    fix plugins parse

--- modules/login.js
+++ modules/login.js
@@ -2,17 +2,19 @@ let login_json = location.hash.substring(1) || "/auth/login_json";
 
 async function init() {
     let result = await fetch(login_json).then(r=>r.json());
-    console.log(result);
 
-    if(Object.keys(result).length < 2) {
+    if(!result.plugins)
+        throw "no plugins in the result";
+
+    if(Object.keys(result.plugins).length < 2) {
         // location.href = Object.values(result)[0];
-        console.log(Object.values(result)[0]);
+        console.log(Object.values(result.plugins)[0]);
         return;
     }
 
     let list = document.getElementById("login_list");
-    for(let plugin_name in result) {
-        let plugin_url = result[plugin_name];
+    for(let plugin_name in result.plugins) {
+        let plugin_url = result.plugins[plugin_name];
 
         let li = document.createElement("li");
         li.innerHTML=`<a href="${plugin_url}">${plugin_name}</a>`;
